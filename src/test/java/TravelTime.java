3ad735596ca5fcbf26785dd36a91073b8e368f17
qwer083 <Qwertyiop890>
2022-10-31 10:28:31 +0300

    final

--- src/test/java/TravelTime.java
+++ src/test/java/TravelTime.java
@@ -9,10 +9,6 @@ public class TravelTime {
         double e = c * d;
         double scale_two = Math.pow(10, 1);
         double result_two = Math.ceil(e * scale_two) / scale_two;
-        String s = "расстояние в километрах от дома до офиса";
-        String f = "средняя скорость такси";
-        String h = "время за которое тестировщик добирается от дома до офиса";
-        String j = "расстояние в километрах от дома до офиса";
         System.out.println("расстояние в километрах от дома до офиса: " + a);
         System.out.println("средняя скорость такси: " + b);
         System.out.println("время за которое тестировщик добирается от дома до офиса: " + (result));
